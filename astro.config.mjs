b667e1858b62cef9a4c27d22a54b2063999d21ee
Svec Antal <anton.svec13@gmail.com>
2026-02-27 14:21:50 +0100

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://Psychol0g1st.github.io',
-	base: '/cpp_learning',
 	integrations: [
 		starlight({
 			title: 'My Docs',
